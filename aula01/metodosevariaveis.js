6a4e186d5bbf18a808d2d09feb5f93f4d0082424
octaviomoraes <otavioaraujo955@gmail.com>
2024-03-20 08:31:50 -0300

    Criação do priemiro arquivo javascript

---
+++ aula01/metodosevariaveis.js
@@ -0,0 +1,9 @@
+var variavel1 = "Salve";
+let variavel2 = 3;
+const variavel3 = 8;
+
+console.log(
+    "Livre para poder buscar o meu lugar ao sol"
+);
+
+console.log(variavel1, "Livre para poder buscar o meu lugar ao sol");
